2236685c42ebffa2a7c8a32741df31928025deaa
Wang Qiu <winsonwq@gmail.com>
2013-11-11 11:38:47 +0800

    make karma-e2e-dsl as a karma-plugin.

---
+++ index.js
@@ -0,0 +1,14 @@
+var createPattern = function(path) {
+  return { pattern: path, included: true, served: true, watched: false };
+};
+
+var initDsl = function(files) {
+  files.unshift(createPattern(__dirname + '/dist/karma-e2e-dsl.min.js'));
+  files.unshift(createPattern(__dirname + '/jquery.min.js'));
+};
+
+initDsl.$inject = ['config.files'];
+
+module.exports = {
+  'framework:karma-e2e-dsl': ['factory', initDsl]
+};
